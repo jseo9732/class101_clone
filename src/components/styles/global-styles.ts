527ba3a34479952182204c93fa751eb279eb2598
jisu Seo <jseo9732@gmail.com>
2022-01-11 02:22:12 +0900

    Chore: 추가 폰트

--- src/components/styles/global-styles.ts
+++ src/components/styles/global-styles.ts
@@ -10,8 +10,53 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   * {
+    margin: 0;
+    padding: 0;
     box-sizing: border-box;
   }
+
+  @font-face {
+    font-family: 'Pretendard-Black';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Black.woff') format('woff');
+    font-weight: 900;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Pretendard-EB';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-ExtraBold.woff') format('woff');
+    font-weight: 800;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Pretendard-B';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff') format('woff');
+    font-weight: 700;
+    font-style: normal;
+  }
+  @font-face {
+      font-family: 'Pretendard-M';
+      src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Medium.woff') format('woff');
+      font-weight: 500;
+      font-style: normal;
+  }
+  @font-face {
+    font-family: 'Pretendard-R';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
+  @font-face {
+    font-family: 'Pretendard-L';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Light.woff') format('woff');
+    font-weight: 300;
+    font-style: normal;
+}
+  @font-face {
+    font-family: 'Pretendard-Th';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Thin.woff') format('woff');
+    font-weight: 100;
+    font-style: normal;
+}
 `;
 
 export default GlobalStyle;
