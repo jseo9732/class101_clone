cd5accd3961c640f7f5d06e29a3cb837fcdc9475
jisu Seo <jseo9732@gmail.com>
2022-01-10 20:13:51 +0900

    styled-components 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'components/styles/theme';
+import GlobalStyle from 'components/styles/global-styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
