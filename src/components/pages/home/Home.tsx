053e047498ef2ed06c9de61dbad2e48f308b48cd
jisu Seo <jseo9732@gmail.com>
2022-01-15 19:54:29 +0900

    Design: 추가 섹션

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,6 +1,35 @@
 import React from 'react';
 import * as style from 'components/pages/home/Style';
+import TimeDealData from 'components/pages/home/data/timeDeal.json';
+import TimeDeal from 'components/pages/home/components/TimeDeal';
+import SectionHeader from './components/SectionHeader';
 
 export default function Home() {
-  return <style.HomeContainer></style.HomeContainer>;
+  return (
+    <style.HomeContainer>
+      <style.OriginalInitialRenderingSections>
+        <SectionHeader path="/time-deal?type=klass" title="오늘의 특가! TIME DEAL" />
+        <style.SwiperContainer>
+          {TimeDealData.time_deal.map((data) => (
+            <TimeDeal key={data.id} data={data} />
+          ))}
+        </style.SwiperContainer>
+      </style.OriginalInitialRenderingSections>
+
+      <style.OriginalInitialRenderingSections>
+        <SectionHeader path="" title="MD 추천 클래스" />
+        <style.SwiperContainer>잠시</style.SwiperContainer>
+      </style.OriginalInitialRenderingSections>
+
+      <style.OriginalInitialRenderingSections>
+        <SectionHeader path="/events?index=0&onGoingOnly=true&status=ENTIRE" title="진행중인 인기 이벤트" />
+        <style.SwiperContainer>잠시</style.SwiperContainer>
+      </style.OriginalInitialRenderingSections>
+
+      <style.OriginalInitialRenderingSections>
+        <SectionHeader path="/products/preview/list" title="오픈 예정 클래스" />
+        <style.SwiperContainer>잠시</style.SwiperContainer>
+      </style.OriginalInitialRenderingSections>
+    </style.HomeContainer>
+  );
 }
--- src/components/pages/home/Style.ts
+++ src/components/pages/home/Style.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components';
 
 export const HomeContainer = styled.div``;
+
+export const OriginalInitialRenderingSections = styled.div`
+  max-width: 1176px;
+  width: 100%;
+  margin: 0 auto;
+  margin-top: 72px;
+`;
+
+export const SwiperContainer = styled.div`
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+  margin-top: 24px;
+  display: flex;
+  flex-direction: row;
+`;
