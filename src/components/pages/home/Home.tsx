31e91ff080102979742e28630b2b092d257526a9
jisu Seo <jseo9732@gmail.com>
2022-01-16 04:41:28 +0900

    Feat: 추가 버튼 스크롤

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -3,6 +3,7 @@ import * as style from 'components/pages/home/Style';
 import TimeDealData from 'components/pages/home/data/timeDeal.json';
 import TimeDeal from 'components/pages/home/components/TimeDeal';
 import SectionHeader from './components/SectionHeader';
+import { Icon } from '@class101/ui';
 
 export default function Home() {
   return (
@@ -10,26 +11,48 @@ export default function Home() {
       <style.OriginalInitialRenderingSections>
         <SectionHeader path="/time-deal?type=klass" title="오늘의 특가! TIME DEAL" />
         <style.SwiperContainer>
-          {TimeDealData.time_deal.map((data) => (
-            <TimeDeal key={data.id} data={data} />
-          ))}
+          <style.SwipeBtnL onClick={slide_L}>
+            <Icon.ChevronLeft size={24} />
+          </style.SwipeBtnL>
+          <style.SwiperBox id="swiper">
+            {TimeDealData.time_deal.map((data) => (
+              <TimeDeal key={data.id} data={data} />
+            ))}
+          </style.SwiperBox>
+          <style.SwipeBtnR onClick={slide_R}>
+            <Icon.ChevronRight size={24} />
+          </style.SwipeBtnR>
         </style.SwiperContainer>
       </style.OriginalInitialRenderingSections>
 
       <style.OriginalInitialRenderingSections>
         <SectionHeader path="" title="MD 추천 클래스" />
-        <style.SwiperContainer>잠시</style.SwiperContainer>
+        <style.SwiperBox>잠시</style.SwiperBox>
       </style.OriginalInitialRenderingSections>
 
       <style.OriginalInitialRenderingSections>
         <SectionHeader path="/events?index=0&onGoingOnly=true&status=ENTIRE" title="진행중인 인기 이벤트" />
-        <style.SwiperContainer>잠시</style.SwiperContainer>
+        <style.SwiperBox>잠시</style.SwiperBox>
       </style.OriginalInitialRenderingSections>
 
       <style.OriginalInitialRenderingSections>
         <SectionHeader path="/products/preview/list" title="오픈 예정 클래스" />
-        <style.SwiperContainer>잠시</style.SwiperContainer>
+        <style.SwiperBox>잠시</style.SwiperBox>
       </style.OriginalInitialRenderingSections>
     </style.HomeContainer>
   );
 }
+
+const slide_L = () =>
+  document.getElementById('swiper')?.scrollBy({
+    top: 0,
+    left: -300,
+    behavior: 'smooth',
+  });
+
+const slide_R = () =>
+  document.getElementById('swiper')?.scrollBy({
+    top: 0,
+    left: 300,
+    behavior: 'smooth',
+  });
--- src/components/pages/home/Style.ts
+++ src/components/pages/home/Style.ts
@@ -4,12 +4,42 @@ export const HomeContainer = styled.div``;
 
 export const OriginalInitialRenderingSections = styled.div`
   max-width: 1176px;
-  width: 100%;
   margin: 0 auto;
   margin-top: 72px;
 `;
 
 export const SwiperContainer = styled.div`
+  position: relative;
+`;
+
+export const SwipeBtnL = styled.button`
+  width: 40px;
+  height: 46px;
+  position: absolute;
+  border-radius: 3px;
+  transition: background-color 0.1s ease 0s;
+  border: 0;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background-color: transparent;
+  display: inline-flex;
+  top: calc((100% - 172px) / 2);
+  transform: translateY(-50%) translateX(-52px);
+  &:hover {
+    background-color: rgb(248, 248, 249);
+  }
+  &.disabled {
+    display: none;
+  }
+`;
+
+export const SwipeBtnR = styled(SwipeBtnL)`
+  right: 0px;
+  transform: translateY(-50%) translateX(52px);
+`;
+
+export const SwiperBox = styled.div`
   position: relative;
   overflow: hidden;
   z-index: 1;
